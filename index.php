44ffebed0339ed9e5f253b6ff58c239a4304e5d2
휘동 배 <hwidong@hwidong-ui-MacBook-Air.local>
2013-03-04 14:52:35 +0900

    shape is formed

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 ini_set("display_errors", "1");
 include_once('./db.php');
+include_once('./room_shape.php');
 ?>
 
 <!DOCTYPE html>
@@ -10,10 +11,50 @@ include_once('./db.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="css/main.css" rel="stylesheet" media="screen">
   </head>
   <body>
-    <h1>Hello, world!</h1>
-    <?php include('./table.php');?>
+  	<div class="container-fluid">
+  		<div class="row-fluid">
+  			<div id="left-container" class="span6">
+  				<div id="4th-floor">  					
+  					<h2>4th floor - East Wing</h2>
+  					<div id="4th-floor-top-rooms" class="row-fluid">
+  						<?php showRoom(4424, TRUE);?>
+  						<?php showRoom(4423);?>
+  						<?php showRoom(4422);?>
+  					</div>
+  					<div class="clearfix"></div>
+  					<div id="4th-floor-bottom-rooms" class="rooms-flipped row-fluid">
+  						<?php showRoom(4425);?>
+  						<?php showRoom(4426, TRUE, 4, TRUE);?>
+  						<?php showRoom(4427, TRUE, 4, TRUE);?>
+  					</div>
+  				</div>
+  				<div id="2nd-floor">
+  					<h2>2nd floor</h2>
+  					<div id="2nd-floor" class="row-fluid">
+  						<div class="span2"></div>
+  						<div class="2nd-floor-room">
+  							<?php showRoom(2201, TRUE, 8);?>	
+  						</div>  						
+  					</div>
+  				</div>		
+  			</div>
+  			<div id="right-continaer" class="span4">
+  				<div id="student-table">
+  					<?php include('./table.php');?>
+  				</div>
+  				<div id="generating-policy">
+  					<h2>Generating Policy</h2>
+  				</div>
+  				<div id="generate-box">
+  					<button class="btn btn-large btn-primary" type="button">Generate!</button>
+  				</div>
+  							
+  			</div>
+  		</div>
+  	</div>
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
   </body>
---
+++ room_shape.php
@@ -0,0 +1,59 @@
+<?php
+function showRoom($number, $active=FALSE, $size=4, $flipped=FALSE){
+	if ($active==TRUE) {
+		echo 
+		'<div id="room-'.$number.'" class="active-room span'.$size.'">
+			<h4 class="room-label">'.$number.'</h4>
+			<div id="'.$number.'-radio" class="btn-group" data-toggle="buttons-radio">
+				<label class="radio-description"># of members</label>			
+				<button type="button" id="'.$number.'-radio-3" class="btn btn-mini active">3</button>
+				<button type="button" id="'.$number.'-radio-4" class="btn btn-mini">4</button>
+				<button type="button" id="'.$number.'-radio-5" class="btn btn-mini">5</button>
+			</div>
+			<div id="'.$number.'-box" class="room-shape">';
+		if ($flipped==FALSE) {
+			echo
+			'	<div class="seats seats-1st clearfix">
+					<div id="'.$number.'-seat1" class="seat-box seat-left">Seat1</div>
+					<div id="'.$number.'-seat2" class="seat-box seat-right">Seat2</div>				
+				</div>
+				<div class="seats seats-2nd clearfix">
+					<div id="'.$number.'-seat3" class="seat-box seat-left">Seat3</div>
+					<div id="'.$number.'-seat4" class="seat-box seat-right">Seat4</div>				
+				</div>
+				<div class="seats seats-3rd clearfix">
+					<div id="'.$number.'-seat5" class="seat-box seat-left">Seat5</div>
+				</div>				
+			</div>
+		</div>';
+		}
+		else {
+			echo
+			'	<div class="seats seats-3rd clearfix">
+					<div id="'.$number.'-seat5" class="seat-box seat-left">Seat5</div>
+				</div>				
+				<div class="seats seats-2nd clearfix">
+					<div id="'.$number.'-seat3" class="seat-box seat-left">Seat3</div>
+					<div id="'.$number.'-seat4" class="seat-box seat-right">Seat4</div>				
+				</div>
+				<div class="seats seats-1st clearfix">
+					<div id="'.$number.'-seat1" class="seat-box seat-left">Seat1</div>
+					<div id="'.$number.'-seat2" class="seat-box seat-right">Seat2</div>				
+				</div>
+			</div>
+		</div>';
+		}
+			
+	}
+	else {
+		echo 
+		'<div id="room-'.$number.'" class="inactive-room span'.$size.'">
+			<h4 class="room-label">'.$number.'</h4>
+			<div class="hidden-radio"></div>
+			<div id="'.$number.'-box" class="room-shape"></div>
+		</div>';
+	}
+	
+}
+?>
+
